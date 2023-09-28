06a5c7774a5066526d034abc3d001245b8832608
Loncex <silvacriollo@gmail.com>
2023-09-28 00:29:27 -0300

    Version 1: Login - Pantalla de Carga

---
+++ app/src/main/java/com/redsystem/app_universidad/Pantalla_Carga.java
@@ -0,0 +1,27 @@
+package com.redsystem.app_universidad;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class Pantalla_Carga extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pantalla_carga);
+
+        int Tiempo = 3000;
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(Pantalla_Carga.this, MainActivity.class));
+                finish();
+
+            }
+        },Tiempo);
+    }
+}
